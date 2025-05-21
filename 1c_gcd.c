9f6adb743ea1e10a61453dca3ca75000fa356e43
Shiva Dhanush <dhanushshiva2003@gmail.com>
2025-05-21 14:28:40 +0530

    Update 1c_gcd.c

--- 1c_gcd.c
+++ 1c_gcd.c
@@ -1,5 +1,7 @@
 // GCD using prime factorization
 #include <stdio.h>
+#include <time.h>
+
 
 int gcd(int a, int b) {
     int gcd = 1;
@@ -24,10 +26,19 @@ int gcd(int a, int b) {
 int main() {
     int num1, num2;
 
+    clock_t start = clock();
+
+
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
 
     printf("The GCD of %d and %d is: %d\n", num1, num2, gcd(num1, num2));
+
+    clock_t end = clock();
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Time taken is %f ms\n", time_taken*1000);
 
     return 0;
 }
